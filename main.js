20a2fe7d7f1afba86e2c79744208700762d0cb0a
ricky-bruner <alteredecho07@yahoo.com>
2018-07-17 11:19:59 -0500

    added notes from Meg

--- main.js
+++ main.js
@@ -24,3 +24,48 @@ function loadData(nameTag){
 }
 
 console.log(loadData("Ricky"));
+
+// BEGIN MEG'S NOTES FROM 7/17 LECTURE
+
+let table = {
+    legs: 4,
+    maker: "Target",
+    color: "brown",
+    height: "12'",
+    material: "wood",
+    move: function(movement){
+        console.log(`This ${this.color} table will ${movement}!`);
+    }
+};
+
+table.move("dance");
+
+table.color = "black";
+table.move("dance");
+
+
+
+const sweetFactory = function(topping){
+    const sweet = Object.create({}, {
+        topping: {
+            value: topping
+        },
+        move: {
+            value: function(arg){
+                console.log(`The sweet will ${arg}`);
+            }
+        }
+    });
+    return sweet;
+};
+
+let arrayOsweets = [];
+
+arrayOsweets.push(sweetFactory("chocolate"), sweetFactory("vanilla"));
+
+console.log(arrayOsweets);
+
+
+
+// END 7/17 LECTURE NOTES
+
